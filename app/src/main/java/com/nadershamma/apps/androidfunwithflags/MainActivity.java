65abd926c1ae0b33496e8db05438500cc456a8c9
Nader Al-Shamma <hello@nadershamma.com>
2018-07-02 16:58:59 +0300

    initial MainActivity Class setup

--- app/src/main/java/com/nadershamma/apps/androidfunwithflags/MainActivity.java
+++ app/src/main/java/com/nadershamma/apps/androidfunwithflags/MainActivity.java
@@ -1,13 +1,26 @@
 package com.nadershamma.apps.androidfunwithflags;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
+import android.content.pm.ActivityInfo;
+import android.content.res.Configuration;
 import android.os.Bundle;
-
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String CHOICES = "pref_numberOfChoices";
+    public static final String REGIONS = "pref_regionsToInclude";
+    private boolean deviceIsPhone = true;
+    private boolean preferencesChanged = true;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,27 +29,90 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        // set default values in the app's SharedPreferences
+        PreferenceManager.setDefaultValues(this, R.xml.preferences, false);
+
+        // Register a listener for shared preferences changes
+        PreferenceManager.getDefaultSharedPreferences(this)
+                .registerOnSharedPreferenceChangeListener(preferencesChangeListener);
+
+        // if the device is a tablet set deviceIsPhone to false
+        int screenSize = getResources().getConfiguration().screenLayout &
+                Configuration.SCREENLAYOUT_SIZE_MASK;
+
+        if (screenSize == Configuration.SCREENLAYOUT_SIZE_LARGE ||
+                screenSize == Configuration.SCREENLAYOUT_SIZE_XLARGE) {
+            deviceIsPhone = false;
+        }
+
+        if (deviceIsPhone) {
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        if (preferencesChanged) {
+            MainActivityFragment quizFragment = (MainActivityFragment) getSupportFragmentManager()
+                    .findFragmentById(R.id.quizFragment);
+            // TODO call MainActivityFragment Methods
+            // Page 143
+            preferencesChanged = false;
+        }
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+        int orientation = getResources().getConfiguration().orientation;
+
+        if (orientation == Configuration.ORIENTATION_PORTRAIT) {
+            // Inflate the menu; this adds items to the action bar if it is present.
+            getMenuInflater().inflate(R.menu.menu_main, menu);
+            return true;
+        } else {
+            return false;
+        }
+
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
+        Intent preferencesIntent = new Intent(this, SettingsActivity.class);
+        startActivity(preferencesIntent);
         return super.onOptionsItemSelected(item);
     }
+
+    private OnSharedPreferenceChangeListener preferencesChangeListener = new OnSharedPreferenceChangeListener() {
+        @Override
+        public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+            preferencesChanged = true;
+
+            MainActivityFragment quizFragment = (MainActivityFragment) getSupportFragmentManager()
+                    .findFragmentById(R.id.quizFragment);
+
+            if (key.equals(REGIONS)) {
+                // TODO add quizFragment methods
+                // page 151
+            } else if (key.equals(CHOICES)) {
+                Set<String> regions = sharedPreferences.getStringSet(REGIONS, null);
+                if (regions != null && regions.size() > 0) {
+                    // TODO add quizFragment methods
+                    // page 151
+                } else {
+                    SharedPreferences.Editor editor = sharedPreferences.edit();
+                    regions.add(getString(R.string.default_region));
+                    editor.putStringSet(REGIONS, regions);
+                    editor.apply();
+
+                    Toast.makeText(MainActivity.this, R.string.default_region_message,
+                            Toast.LENGTH_LONG).show();
+                }
+            }
+
+            Toast.makeText(MainActivity.this, R.string.restarting_quiz,
+                    Toast.LENGTH_SHORT).show();
+        }
+    };
 }
