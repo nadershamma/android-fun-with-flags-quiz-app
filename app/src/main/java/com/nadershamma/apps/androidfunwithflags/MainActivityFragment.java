a8ce01163dff94822ca02daf3259e0ba2aefea24
Nader Al-Shamma <hello@nadershamma.com>
2018-07-02 17:31:28 +0300

    added MainActivityFragment fields and dependencies

--- app/src/main/java/com/nadershamma/apps/androidfunwithflags/MainActivityFragment.java
+++ app/src/main/java/com/nadershamma/apps/androidfunwithflags/MainActivityFragment.java
@@ -1,15 +1,66 @@
 package com.nadershamma.apps.androidfunwithflags;
 
-import android.support.v4.app.Fragment;
+import java.io.IOException;
+import java.io.InputStream;
+import java.security.SecureRandom;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
+import android.content.res.AssetManager;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.Fragment;
+import android.os.Handler;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewAnimationUtils;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+
+import android.support.constraint.ConstraintLayout;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
+
+    private static final String TAG = "FlagQuiz Activity";
+    private static final int FLAGS_IN_QUIZ = 10;
+
+    private List<String> fileNameList;
+    private List<String> quizCountriesList;
+    private Set<String> regionsSet;
+    private String correctAnswer;
+    private int totalGuesses;
+    private int correctAnswers;
+    private int guessRows;
+    private SecureRandom random;
+    private Handler handler;
+    private Animation shakeAnimation;
+
+    private ConstraintLayout quizConstraintLayout;
+    private TextView questionNumberTextView;
+    private ImageView flagImageView;
+    private TableLayout answersTableLayout;
+    private TableRow[] guessTableRows;
+    private TextView answerTextView;
 
     public MainActivityFragment() {
     }
